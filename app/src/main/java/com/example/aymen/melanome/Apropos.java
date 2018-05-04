6b22d1beb42a1b6f97237cebfd859a06a5a4d348
MahmoudAymen <aymen_mahmoud_fsm@hotmail.com>
2018-05-05 00:54:09 +0100

    commit 7

--- app/src/main/java/com/example/aymen/melanome/Apropos.java
+++ app/src/main/java/com/example/aymen/melanome/Apropos.java
@@ -1,13 +1,58 @@
 package com.example.aymen.melanome;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Apropos extends AppCompatActivity {
+public class Apropos extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apropos);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.galleryOrCmera:
+                Intent i=new Intent(Apropos.this,CameraOrGallery.class);
+                startActivity(i);
+                break;
+            case R.id.aprop:
+                Intent i1=new Intent(Apropos.this,Apropos.class);
+                startActivity(i1);
+                break;
+            default:
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
